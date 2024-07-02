f8e4ff453bfa92c12b167aef398c0b223b714009
theeduke <oscarian528@gmail.com>
2024-07-02 06:41:53 -0500

    update

--- Round2/app.py
+++ Round2/app.py
@@ -10,22 +10,32 @@ app = Flask(__name__)
 GEOIP_API_KEY = os.getenv('GEOIP_API_KEY')
 WEATHER_API_KEY = os.getenv('WEATHER_API_KEY')
 
-@app.route('/api/hello', methods=['GET'])
-def hello():
-    visitor_name = request.args.get('visitor_name', 'Guest')
-    client_ip = request.remote_addr
+@app.route('/api/hello/', methods=['GET'])
+def hello():    
+    visitor_name = request.args.get('visitor_name')
+    print(request.args.get('visitor_name'))
+    if visitor_name:
+        visitor_name = visitor_name.strip('"')
 
-    # Get location from IP address using GeoIP API
-    geoip_url = f'http://api.ipstack.com/{client_ip}?access_key={GEOIP_API_KEY}'
+    client_ip = request.headers.get('X-Forwarded-For', request.remote_addr).split(',')[0].strip()
+
+#     # Get location from IP address using GeoIP API/ exhausted free tier
+    # geoip_url = f'http://api.ipstack.com/{client_ip}?access_key={GEOIP_API_KEY}'
+    # geoip_response = requests.get(geoip_url).json()
+    # location = geoip_response.get('city','Unknown')
+        #using ip-api.com
+    geoip_url = f'http://ip-api.com/json/{client_ip}'
     geoip_response = requests.get(geoip_url).json()
-    location = geoip_response.get('city', 'Unknown')
+    location = geoip_response.get('city')
 
-    # Get weather data for the location using Weather API
+
+#     # Get weather data for the location using Weather API
     weather_url = f'http://api.openweathermap.org/data/2.5/weather?q={location}&units=metric&appid={WEATHER_API_KEY}'
     weather_response = requests.get(weather_url).json()
     temperature = weather_response['main']['temp'] if 'main' in weather_response else 'Unknown'
 
     greeting = f"Hello, {visitor_name}!, the temperature is {temperature} degrees Celsius in {location}"
+
 
     response = {
         "client_ip": client_ip,
